f12e0bad6d7a419ab23c374e3bf33ad64df059d6
Bohdan Dovhan <bohdan.dovhan@gmail.com>
2021-01-26 19:44:06 +0200

    Include the rebuild script

---
+++ SimpleDataTable/rebuildAll.sh
@@ -0,0 +1,15 @@
+hub=$(sfdx config:get defaultdevhubusername | awk '/^==|^──|^Name/ {next}{print $2}')
+sfdx config:set defaultdevhubusername=$1
+default=$(sfdx config:get defaultusername | awk '/^==|^──|^Name/ {next}{print $2}')
+sfdx force:config:set defaultusername=$2
+
+./createPackageVersion.sh $1 $2 "Base Setup" baseSetup "Setup.default();" "" $3
+./createPackageVersion.sh $1 $2 "Simple Data Table" simpleDataTable "" "" $3
+./createPackageVersion.sh $1 $2 "Data Table" dataTable "" "" $3
+
+namespace = $(cat sfdx-project.json | jq '.namespace' -r)
+sfdx force:org:open -p $namespace/SimpleDataApp.app
+sfdx force:org:open -p $namespace/DataTableTestApp.app
+
+sfdx config:set defaultusername=$default
+sfdx config:set defaultdevhubusername=$hub
